b8c716da2815bac5b523e8801b5fbc6558ac47d4
Davis Raymond Muro <davisraymondmuro@outlook.com>
2020-06-25 14:05:15 +0300

    Add test to ensure django services are running after playbook runs

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,3 +13,16 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+def test_django_service(host):
+    django_app = host.service("django_example_app")
+    assert django_app.is_running
+    assert django_app.is_enabled
+
+    celeryd_django_app = host.service("celeryd-django_example_app")
+    assert celeryd_django_app.is_running
+    assert celeryd_django_app.is_enabled
+
+    celerybeat_django_app = host.service("celerybeat-django_example_app")
+    assert celeryd_django_app.is_running
+    assert celeryd_django_app.is_enabled
